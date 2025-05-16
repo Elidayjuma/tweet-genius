7f5c01db3c2c26e2af8a7ee17ecc45dcc6659055
elidayjuma <elidejuma@gmail.com>
2025-05-16 19:20:17 +0300

    rectify nextjs config url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["interviewprep.elidayjuma.com"],
+      allowedOrigins: ["tweetgenius.elidayjuma.com"],
     },
   },
 };
